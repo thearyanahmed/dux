1dd37faa7d8e5964205dd634a01ceb760df8b0a2
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2024-03-30 02:31:26 +0100

    Add read from dux.json

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ The name `dux` is from Disk Utility X. Couldn't find a better name.
 - The program should be able to read a config file and move files and folders according to the map
 - Should be a binary executable
 - The config should support regex
-
+- The organize command should be extensible with only --ft=png,jpg --type=images,dir // eg: images would be from the custom mapping again
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-use anyhow::Ok;
+use anyhow::{Context, Ok, Result};
 use clap::{Parser, Subcommand};
+
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs;
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    base: String,
+    map: HashMap<String, String>,
+    alias: HashMap<String, String>,
+}
+
+impl Config {
+    pub fn print(&self) {
+        println!("base: {}", self.base);
+        println!("map:");
+        for (key, value) in &self.map {
+            println!("  {}: {}", key, value);
+        }
+        println!("alias:");
+        for (key, value) in &self.alias {
+            println!("  {}: {}", key, value);
+        }
+    }
+}
 
 /// dux (Disk Utility X)
 #[derive(Parser, Debug)]
@@ -20,6 +45,8 @@ enum Command {
 
     /// Displays currently loaded config map
     Config,
+    /// Reads config (testing purpose only)
+    ReadConfig,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -32,6 +59,25 @@ fn main() -> anyhow::Result<()> {
         Command::Config => {
             println!("display config map");
         }
+
+        Command::ReadConfig => {
+            let config = read_config()?;
+
+            config.print();
+        }
     }
     Ok(())
+}
+
+// TODO: config path should be a parameter
+pub fn read_config() -> Result<Config> {
+    // Note: need to add absolute path. The target is running from target/debug/dux. When it looks
+    // for dux.json inside that, it would not find it.
+    let dux_conf = fs::read_to_string("/Users/thearyanahmed/web/projects/dux/dux.json")
+        .context("failed to read dux.json")?;
+
+    // Parse the JSON string into a Config struct
+    let config: Config = serde_json::from_str(&dux_conf).context("failed to map config file")?;
+
+    Ok(config)
 }
