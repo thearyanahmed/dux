57d756e3d582fec73d450532e4b937eca899a054
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2024-03-30 03:17:59 +0100

    Add script to generate fake files

---
+++ make_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define an array of file extensions
+extensions=("png" "jpg" "jpeg" "mp4" "php" "py" "rs" "docs" "dox" "xl")
+
+# Define the number of files to create
+num_files=5
+
+# Create tests directory if it doesn't exist
+mkdir -p tests
+
+# Loop through each extension and create files inside tests directory
+for ext in "${extensions[@]}"; do
+    for (( i=1; i<=$num_files; i++ )); do
+        touch "tests/file$i.$ext"
+    done
+done
+
+echo "files created successfully inside tests directory."
